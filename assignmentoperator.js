ad78511305681ac41f0c7bb8caa60fd0021ef795
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-03-20 22:53:41 +0500

    Add files via upload

---
+++ assignmentoperator.js
@@ -0,0 +1,26 @@
+"use strict";
+//Assignment Operator
+/* = Assignment
+==  Equal to
+=== Equal to + check data type*/
+// Assignment Operator
+let n1 = 3;
+console.log(n1 == 3);
+console.log(n1 === 3);
+//console.log(n1=="3");
+//console.log(n1==="3");
+console.log(n1 !== 3);
+// Comparision Operator
+//let n1=3;
+//Console.log("ratan lal" == "ratan lal");
+//Console.log(n1 !==10;
+//Console.log(n1 > 2);
+//Console.log(n1 < 2);
+//Console.log(n1 >= 2);
+//Console.log(n1 <= 2);
+// Comparision Operator
+//let n1=3;
+//AND (&&) command = both condition should be true
+//Console.log((n1====3) && (n1=>2))
+//OR (||) command = any one condition should be true
+//Console.log((n1====3) || (n1=>2))
